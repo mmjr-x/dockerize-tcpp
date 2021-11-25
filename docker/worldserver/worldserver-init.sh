11734a5caab9afaf9e8103d1fc67a999244ad25c
mmjr-x <2429824+mmjr-x@users.noreply.github.com>
2021-11-26 00:25:39 +0100

    Added ability to merge custom changes into the worldserver.conf when the container is ran and optimised the base docker container a bit

--- docker/worldserver/worldserver-init.sh
+++ docker/worldserver/worldserver-init.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
 
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+# Process mounted configs
+# https://stackoverflow.com/a/7810345/13620003
+[ -f "/tcpp-configs/worldserver-changes.conf" ] && crudini --merge /tcpp-server/etc/worldserver.conf < /tcpp-configs/worldserver-changes.conf
 
 # Uncomment this if you want to just run the container indefinitely (to inspect it or something)
 tail -f /dev/null
